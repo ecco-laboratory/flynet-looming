fd6dcab80484c0546c705a41bc8edad90e856625
monicathieu <mthieu@emory.edu>
2022-08-31 11:02:57 -0400

    add cropbox nsd to pillow

--- code/image_utils.py
+++ code/image_utils.py
@@ -6,15 +6,18 @@ import requests
 from io import BytesIO
 
 # %%
-# function definition
+# read_and_preprocess_image
 
-def read_and_preprocess_image(path):
+def read_and_preprocess_image(path, cropbox = None):
     if path.startswith('http'):
         # use requests and io.BytesIO to pull from web
         img = Image.open(BytesIO(requests.get(path).content))
     else:
         img = Image.open(path)
     
+    # expect pre-formatted pillow cropbox
+    if cropbox is not None:
+        img = img.crop(cropbox)
     # resize and then render as numpy array
     # but leave the image in 0-255 RGB space as that's also how Matlab imread returns imgs
     # TODO: soft-code the dimensions to get the height and width of the img from the model
@@ -24,3 +27,24 @@ def read_and_preprocess_image(path):
     img = np.moveaxis(img, -1, 0)
 
     return img
+
+# %%
+# cropbox_nsd_to_pillow
+def cropbox_nsd_to_pillow(size, cropbox):
+    # expect size to come in from the COCO metadata
+    # expect cropbox to come in from the NSD stim dataframe
+
+    if sum(cropbox[0:2]) == 0:
+        upper = 0
+        left = size[0] * cropbox[2]
+        lower = size[1]
+        right = size[0] - (size[0] * cropbox[3])
+    else:
+        upper = size[1] * cropbox[0]
+        left = 0
+        lower = size[1] - (size[1] * cropbox[1])
+        right = size[0]
+    
+    return (left, upper, right, lower)
+
+# %%
