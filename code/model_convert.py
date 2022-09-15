938cb1a57459b2012c735f758c252faf1ccc09c7
monicathieu <mthieu@emory.edu>
2022-09-15 13:45:07 -0400

    use conda onnx2torch again, save dict out

--- code/model_convert.py
+++ code/model_convert.py
@@ -1,24 +1,17 @@
 # %%
 # imports, per usual
-# including the local version of onnx2torch
-# presuming the conda-forge version isn't already installed
-# append puts it onto the end of the path (aka lowest priority)
-import sys
 
 import onnx
 import torch
-import torchvision
 from onnx import helper, version_converter
-
-sys.path.append('/Users/mthieu/Repos/onnx2torch')
 from onnx2torch import convert
 
-emonet_path = '../ignore/models/EmoNet.onnx'
+emonet_path = '../ignore/models/'
 # %%
 # load the ONNX version of the model
-emonet_onnx = onnx.load(emonet_path)
+emonet_onnx = onnx.load(emonet_path+'EmoNet.onnx')
 emonet_onnx_13 = version_converter.convert_version(emonet_onnx, 13)
-emonet_torch = convert(emonet_onnx_13)
+emonet_onnx2torch = convert(emonet_onnx_13)
 # %%
-alexnet = torch.hub.load('pytorch/vision:v0.10.0', 'alexnet', pretrained=True)
-# %%
+# write the model dict out
+torch.save(emonet_onnx2torch.state_dict(), emonet_path+'EmoNet.pt')
