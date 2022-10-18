f4c265003d6463db3e9fa8f0148aea13d67b0057
monicathieu <mthieu@emory.edu>
2022-10-18 14:41:53 -0400

    move NSD hdf5 dependencies to new file

--- python/emonet_utils.py
+++ python/emonet_utils.py
@@ -1,26 +1,25 @@
 # %%
 # imports per usual
-from typing import Any, Callable, List, Optional, Tuple, Union
+from typing import Any, Callable, List, Optional, Tuple
 
-import h5py
-import pandas as pd
 import torch
 import torchvision
-from PIL import Image
 from torch import nn
 from torchvision.datasets import VisionDataset
 
 
 # %%
 # Helper function so the Dataset returns the supa raw emo class label
-def get_target_emotion_index(target):
-    target = emonet_output_classes.index(target['emotion'])
-    return torch.Tensor([target]).to(int)
+# Now must feed in the class list as an arg, in the event that you're using fewer than the full Phil 20
+def get_target_emotion_index(target, classes):
+    target = classes.index(target['emotion'])
+    # return torch.Tensor([target]).to(int)
+    return target
 
 # %%
-# Instantiate my shitty little GRU RNN with linear-to-softmax predictor
+# Instantiate another shitty little LSTM RNN with linear-to-softmax predictor
 
-class GRUClassifier(nn.Module):
+class LSTMClassifier(nn.Module):
 
     def __init__(
         self,
@@ -28,9 +27,9 @@ class GRUClassifier(nn.Module):
         hidden_size: int=110,
         num_classes: int=20
     ) -> None:
-        super(GRUClassifier, self).__init__()
+        super(LSTMClassifier, self).__init__()
 
-        self.gru = nn.GRU(input_size=input_size, hidden_size=hidden_size)
+        self.lstm = nn.LSTM(input_size=input_size, hidden_size=hidden_size)
         # I guess I could do this to two linear layers and call that bitch a perceptron
         # Not right now though
         self.classifier = nn.Sequential(
@@ -41,20 +40,75 @@ class GRUClassifier(nn.Module):
 
     def forward(self, x):
         # We don't care about the last hidden state
+        x, _ = self.lstm(x)
+        # Unpack the sequence, and... hopefully this is safe... throw away video lengths
+        x, lens = nn.utils.rnn.pad_packed_sequence(x, batch_first=True)
+        # Use the batch lengths to index the actual end frames for each video
+        # I'm not sure that the last (padded) frame has it carried forward
+        x = torch.stack([x[idx, lens[idx]-1, :] for idx in range(lens.size(dim=0))])
+        x = self.classifier(x)
+ 
+        return x
+
+
+# %%
+# Instantiate my shitty little GRU RNN with linear-to-softmax predictor
+
+class GRUClassifier(nn.Module):
+
+    def __init__(
+        self,
+        input_size: int=4096,
+        hidden_size: int=110,
+        num_classes: int=20,
+        bidirectional: bool=False
+    ) -> None:
+        super(GRUClassifier, self).__init__()
+
+        hidden_size_coef = 2 if bidirectional else 1
+
+        self.gru = nn.GRU(input_size=input_size, hidden_size=hidden_size, bidirectional=bidirectional)
+        # I guess I could do this to two linear layers and call that bitch a perceptron
+        # Not right now though
+        self.classifier = nn.Sequential(
+            nn.Linear(in_features=hidden_size*hidden_size_coef, out_features=num_classes),
+            # Need log probabilities for the loss calculator
+            nn.LogSoftmax(dim=-1)
+        )
+
+    def forward(self, x):
+        # We don't care about the last hidden state
         x, _ = self.gru(x)
         # Unpack the sequence, and... hopefully this is safe... throw away video lengths
-        x = nn.utils.rnn.pad_packed_sequence(x, batch_first=True)
-        # We need to get the final prediction for each video
-        # Hopefully the padding doesn't fuck with the hidden states too bad
-        # that the shorter videos will just be feeding zeros into this
-        # I think the hidden state gets carried forward if the next input is all zeros
-        # But if this doesn't work, we will need to use the batch lengths to index the actual end frames
-        # And not just assume the last (padded) frame has it carried forward
-        x = self.classifier(x[0][:, -1, :])
+        x, lens = nn.utils.rnn.pad_packed_sequence(x, batch_first=True)
+        # Use the batch lengths to index the actual end frames for each video
+        # I'm not sure that the last (padded) frame has it carried forward
+        x = torch.stack([x[idx, lens[idx]-1, :] for idx in range(lens.size(dim=0))])
+        x = self.classifier(x)
  
         return x
 
     
+# %%
+# Instantiate linear classifier (the original head for headless EmoNet)
+
+class LinearClassifier(nn.Module):
+    def __init__(
+        self,
+        input_size: int=4096,
+        num_classes: int=20
+    ) -> None:
+        super().__init__()
+        self.classifier = nn.Sequential(
+            nn.Linear(in_features=input_size, out_features=num_classes),
+            # Need log probabilities for the loss calculator
+            nn.LogSoftmax(dim=-1)
+        )
+    
+    def forward(self, x):
+        x = self.classifier(x)
+        return x
+
 # %%
 # instantiate emonet (from onnx) pytorch class :3
 
@@ -290,6 +344,104 @@ class EmoNetHeadlessVideo(nn.Module):
         return x
 
 # %%
+# torch Dataset class for Alan's video FRAMES
+
+class Cowen2017FrameDataset(VisionDataset):
+    """`Cowen & Keltner (2017) <https://www.pnas.org/doi/full/10.1073/pnas.1702247114>` PyTorch-style Dataset.
+
+    This dataset returns each video frame as a 3D tensor.
+
+    Args:
+        root (string): Enclosing folder where videos are located on the local machine.
+        annFile (string): Path to directory of metadata/annotation CSVs.
+        censorFile (boolean, optional): Censor Alan's "bad" videos? Defaults to True.
+        train (boolean, optional): If True, creates dataset from Kragel et al. (2019)'s training set, otherwise
+            from the testing set. Defaults to True.
+        transform (callable, optional): A function/transform that  takes in an PIL image
+            and returns a transformed version. E.g, ``transforms.PILToTensor``
+        target_transform (callable, optional): A function/transform that takes in the
+            target and transforms it.
+        transforms (callable, optional): A function/transform that takes input sample and its target as entry
+            and returns a transformed version.
+    """
+
+    def __init__(self,
+                 root: str,
+                 annPath: str,
+                 censor: bool = True,
+                 classes: str = None,
+                 train: bool = True,
+                 device: str = 'cpu',
+                 transforms: Optional[Callable] = None,
+                 transform: Optional[Callable] = None,
+                 target_transform: Optional[Callable] = None) -> None:
+        super().__init__(root, transforms, transform, target_transform)
+
+        import os
+
+        import pandas as pd
+
+        self.train = train
+        self.device = device
+
+        # Read in the Cowen & Keltner top-1 "winning" human emotion classes
+        self.labels = pd.read_csv(os.path.join(annPath, 'frame_10fps_metadata.csv'),
+                                   index_col=['video', 'frame'])
+        self.labels = self.labels[self.labels['emotion'].isin(emonet_output_classes)]
+        
+        # Can do it this way bc this metadata has it in a column
+        self.labels = self.labels.query(f"{'' if self.train else '~'}train")
+
+        # Flexibly subsample emotion classes based on user input
+        if classes is not None:
+            self.labels = self.labels[self.labels['emotion'].isin(classes)]
+
+        if censor:
+            # Can do it this way bc this metadata has it in a column
+            self.labels = self.labels.query('~censored')
+
+        # These come out as 2-tuples, mind you
+        self.ids = self.labels.index.to_list()
+    
+    def _load_frame(self, video_id: str, frame_id: str):
+        import os
+
+        video = torchvision.io.read_video(os.path.join(self.root, self.labels.loc[video_id, frame_id]['emotion'], video_id),
+                                          pts_unit='sec')
+        # None of the videos have audio, so discard that from the loaded tuple
+        # Also for convenience, discard dict labeling fps so that the videos look like 4D imgs
+        # with dims frames x channels x height x width ... which is NOT the default order!
+        frames = video[0].permute((0, 3, 1, 2))
+
+        return frames[frame_id]
+    
+    def _load_target(self, video_id, frame_id) -> List[Any]:
+        target = self.labels.loc[video_id, frame_id].to_dict()
+        target['video_id'] = video_id
+        target['frame_id'] = frame_id
+        
+        return target
+    
+    def __getitem__(self, index: int) -> Tuple[Any, Any]:
+        video_id, frame_id = self.ids[index]
+        frame = self._load_frame(video_id, frame_id)
+        target = self._load_target(video_id, frame_id)
+
+        if self.transforms is not None:
+            frame, target = self.transforms(frame, target)
+            
+        frame.to(device=self.device)
+
+        if (self.target_transform is not None) & torch.is_tensor(target):
+            target.to(device=self.device)
+
+        return frame, target
+
+    def __len__(self) -> int:
+        return len(self.ids)
+
+
+# %%
 # Hot and sexy torch Dataset class for Alan's videos
 
 class Cowen2017Dataset(VisionDataset):
@@ -315,6 +467,7 @@ class Cowen2017Dataset(VisionDataset):
                  root: str,
                  annPath: str,
                  censor: bool = True,
+                 classes: str = None,
                  train: bool = True,
                  device: str = 'cpu',
                  transforms: Optional[Callable] = None,
@@ -334,6 +487,10 @@ class Cowen2017Dataset(VisionDataset):
                                    index_col='video')
         self.labels = self.labels[self.labels['emotion'].isin(emonet_output_classes)]
         
+        # Flexibly subsample emotion classes based on user input
+        if classes is not None:
+            self.labels = self.labels[self.labels['emotion'].isin(classes)]
+
         if censor:
             # Truly I wish this was in long form but Alan doesn't like tidy data does he
             censored = pd.read_csv(os.path.join(annPath, 'censored_video_ids.csv'))
@@ -384,132 +541,7 @@ class Cowen2017Dataset(VisionDataset):
         return len(self.ids)
 
 # %%
-# Let me create myself a goddamn torch Dataset class for NSD
-
-class NSDDataset(VisionDataset):
-    """`NSD Stimuli <https://cvnlab.slite.page/p/NKalgWd__F/>` PyTorch-style Dataset.
-    
-    It requires the hdf5 file containing the NSD stimuli to have been downloaded.
-
-    Args:
-        root (string): Folder where hdf5 file is downloaded to.
-        annFile (string): Path to CSV annotation file.
-        shared1000 (boolean): Run using the subset of stimuli that all participants saw? Defaults to False (all 73000 possible stimuli).
-        transform (callable, optional): A function/transform that  takes in an PIL image
-            and returns a transformed version. E.g, ``transforms.PILToTensor``
-        target_transform (callable, optional): A function/transform that takes in the
-            target and transforms it.
-        transforms (callable, optional): A function/transform that takes input sample and its target as entry
-            and returns a transformed version.
-    
-    """
-
-    def __init__(self,
-                 root: str,
-                 annFile: str,
-                 shared1000: bool = False,
-                 transform: Optional[Callable] = None, 
-                 target_transform: Optional[Callable] = None,
-                 transforms: Optional[Callable] = None
-                 ) -> None:
-        super().__init__(root, transforms, transform, target_transform)
-
-        from ast import literal_eval
-
-        # It's allowed to be pandas!
-        # convert cropBox so it reads in as a tuple, not a string of the tuple
-
-        self.metadata = pd.read_csv(annFile, converters = {'cropBox': literal_eval})
-        self.metadata = self.metadata.set_index('Unnamed: 0')
-        if shared1000:
-            self.metadata = self.metadata.query('shared1000')
-        self.hdf = h5py.File(self.root + 'nsd_stimuli.hdf5')
-        self.ids = self.metadata.index.to_list()
-        self.transform = transform
-        self.target_transform = target_transform
-    
-    def _load_image(self, id: int) -> Image.Image:
-        image = Image.fromarray(self.hdf['imgBrick'][id])
-
-        if self.transform is not None:
-            image = self.transform(image)
-
-        return image
-    
-    def _load_target(self, id: int) -> List[Any]:
-        target = self.metadata.loc[id].to_dict()
-
-        if self.target_transform is not None:
-            target = self.target_transform(target)
-
-        return target
-
-    def __getitem__(self, index: int) -> Tuple[Any, Any]:
-        id = self.ids[index]
-        image = self._load_image(id)
-        target = self._load_target(id)
-
-        if self.transforms is not None:
-            image, target = self.transforms(image, target)
-
-        return image, target
-
-    
-    def __len__(self) -> int:
-        return len(self.ids)
-# %%
-# torchvision cocodetection but it reads from web (cursed?)
-
-class CocoDetectionFromWeb(VisionDataset):
-    """`MS Coco Detection <https://cocodataset.org/#detection-2016>`_ Dataset.
-
-    It requires the `COCO API to be installed <https://github.com/pdollar/coco/tree/master/PythonAPI>`_.
-
-    Args:
-        annFile (string): Path to json annotation file.
-        transform (callable, optional): A function/transform that  takes in an PIL image
-            and returns a transformed version. E.g, ``transforms.PILToTensor``
-        target_transform (callable, optional): A function/transform that takes in the
-            target and transforms it.
-        transforms (callable, optional): A function/transform that takes input sample and its target as entry
-            and returns a transformed version.
-    """
-
-    def __init__(
-        self,
-        cocoAnnFile: str,
-        nsdAnnFile: str,
-        transform: Optional[Callable] = None,
-        target_transform: Optional[Callable] = None,
-        transforms: Optional[Callable] = None,
-    ) -> None:
-        super().__init__(transforms, transform, target_transform)
-        from pycocotools.coco import COCO
-
-        self.coco = COCO(cocoAnnFile)
-        self.ids = list(sorted(self.coco.imgs.keys()))
-
-    def _load_image(self, id: int) -> Image.Image:
-        path = self.coco.loadImgs(id)[0]["coco_url"]
-        return Image.open(BytesIO(requests.get(path).content)).convert("RGB")
-
-    def _load_target(self, id: int) -> List[Any]:
-        return self.coco.loadAnns(self.coco.getAnnIds(id))
-
-    def __getitem__(self, index: int) -> Tuple[Any, Any]:
-        id = self.ids[index]
-        image = self._load_image(id)
-        target = self._load_target(id)
-
-        if self.transforms is not None:
-            image, target = self.transforms(image, target)
-
-        return image, target
-
-
-    def __len__(self) -> int:
-        return len(self.ids)
-
+# Phil's 20 EmoNet output classes as global variable
 emonet_output_classes = [
     'Adoration',
     'Aesthetic Appreciation',
---
+++ python/nsd_utils.py
@@ -0,0 +1,137 @@
+# %%
+# imports per usual
+from io import BytesIO
+from typing import Any, Callable, List, Optional, Tuple
+
+import h5py
+import pandas as pd
+import requests
+from PIL import Image
+from torchvision.datasets import VisionDataset
+
+# %%
+# Let me create myself a goddamn torch Dataset class for NSD
+
+class NSDDataset(VisionDataset):
+    """`NSD Stimuli <https://cvnlab.slite.page/p/NKalgWd__F/>` PyTorch-style Dataset.
+    
+    It requires the hdf5 file containing the NSD stimuli to have been downloaded.
+
+    Args:
+        root (string): Folder where hdf5 file is downloaded to.
+        annFile (string): Path to CSV annotation file.
+        shared1000 (boolean): Run using the subset of stimuli that all participants saw? Defaults to False (all 73000 possible stimuli).
+        transform (callable, optional): A function/transform that  takes in an PIL image
+            and returns a transformed version. E.g, ``transforms.PILToTensor``
+        target_transform (callable, optional): A function/transform that takes in the
+            target and transforms it.
+        transforms (callable, optional): A function/transform that takes input sample and its target as entry
+            and returns a transformed version.
+    
+    """
+
+    def __init__(self,
+                 root: str,
+                 annFile: str,
+                 shared1000: bool = False,
+                 transform: Optional[Callable] = None, 
+                 target_transform: Optional[Callable] = None,
+                 transforms: Optional[Callable] = None
+                 ) -> None:
+        super().__init__(root, transforms, transform, target_transform)
+
+        from ast import literal_eval
+
+        # It's allowed to be pandas!
+        # convert cropBox so it reads in as a tuple, not a string of the tuple
+
+        self.metadata = pd.read_csv(annFile, converters = {'cropBox': literal_eval})
+        self.metadata = self.metadata.set_index('Unnamed: 0')
+        if shared1000:
+            self.metadata = self.metadata.query('shared1000')
+        self.hdf = h5py.File(self.root + 'nsd_stimuli.hdf5')
+        self.ids = self.metadata.index.to_list()
+        self.transform = transform
+        self.target_transform = target_transform
+    
+    def _load_image(self, id: int) -> Image.Image:
+        image = Image.fromarray(self.hdf['imgBrick'][id])
+
+        if self.transform is not None:
+            image = self.transform(image)
+
+        return image
+    
+    def _load_target(self, id: int) -> List[Any]:
+        target = self.metadata.loc[id].to_dict()
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return target
+
+    def __getitem__(self, index: int) -> Tuple[Any, Any]:
+        id = self.ids[index]
+        image = self._load_image(id)
+        target = self._load_target(id)
+
+        if self.transforms is not None:
+            image, target = self.transforms(image, target)
+
+        return image, target
+
+    
+    def __len__(self) -> int:
+        return len(self.ids)
+# %%
+# torchvision cocodetection but it reads from web (cursed?)
+
+class CocoDetectionFromWeb(VisionDataset):
+    """`MS Coco Detection <https://cocodataset.org/#detection-2016>`_ Dataset.
+
+    It requires the `COCO API to be installed <https://github.com/pdollar/coco/tree/master/PythonAPI>`_.
+
+    Args:
+        annFile (string): Path to json annotation file.
+        transform (callable, optional): A function/transform that  takes in an PIL image
+            and returns a transformed version. E.g, ``transforms.PILToTensor``
+        target_transform (callable, optional): A function/transform that takes in the
+            target and transforms it.
+        transforms (callable, optional): A function/transform that takes input sample and its target as entry
+            and returns a transformed version.
+    """
+
+    def __init__(
+        self,
+        cocoAnnFile: str,
+        nsdAnnFile: str,
+        transform: Optional[Callable] = None,
+        target_transform: Optional[Callable] = None,
+        transforms: Optional[Callable] = None,
+    ) -> None:
+        super().__init__(transforms, transform, target_transform)
+        from pycocotools.coco import COCO
+
+        self.coco = COCO(cocoAnnFile)
+        self.ids = list(sorted(self.coco.imgs.keys()))
+
+    def _load_image(self, id: int) -> Image.Image:
+        path = self.coco.loadImgs(id)[0]["coco_url"]
+        return Image.open(BytesIO(requests.get(path).content)).convert("RGB")
+
+    def _load_target(self, id: int) -> List[Any]:
+        return self.coco.loadAnns(self.coco.getAnnIds(id))
+
+    def __getitem__(self, index: int) -> Tuple[Any, Any]:
+        id = self.ids[index]
+        image = self._load_image(id)
+        target = self._load_target(id)
+
+        if self.transforms is not None:
+            image, target = self.transforms(image, target)
+
+        return image, target
+
+
+    def __len__(self) -> int:
+        return len(self.ids)
