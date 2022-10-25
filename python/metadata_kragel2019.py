a207193b12231c3ff230be91e740c2224289b3b2
monicathieu <mthieu@emory.edu>
2022-10-25 10:55:04 -0400

    output metadata file for 10fps frames

--- python/metadata_kragel2019.py
+++ python/metadata_kragel2019.py
@@ -19,7 +19,7 @@ else:
 
 stim_path = os.path.join(base_path, 'CowenKeltner/Videos_by_Category')
 metadata_path = '/home/mthieu/Repos/CowenKeltner/metadata'
-
+fps10_path = '/home/mthieu/Repos/CowenKeltner/videos_10fps'
 # %%
 # extract metadata for videos
 # This takes like 1s per video! Not trivial with 2k videos
@@ -30,15 +30,14 @@ video_widths = []
 video_frame_rates = []
 video_durations = []
 video_nframes = []
-for root, pwd, files in os.walk(stim_path):
+for root, pwd, files in os.walk(fps10_path):
     print('Current folder:', root)
     for file in tqdm(files):
         if file.endswith('.mp4'):
             video_ids.append(file)
             path = os.path.join(root, file)
             video_paths.append(path)
-
-            if False:
+            if True:
                 metadata = ffmpeg.probe(path)['streams'][0]
                 video_heights.append(metadata['height'])
                 video_widths.append(metadata['width'])
@@ -58,12 +57,15 @@ video_metadata = pd.DataFrame({
 video_metadata = video_metadata.set_index('video')
 video_metadata['frame_rate_float'] = [float(Fraction(rate)) for rate in video_metadata['frame_rate']]
 
-video_metadata.to_csv(os.path.join(metadata_path, 'video_metadata.csv'))
+video_metadata.to_csv(os.path.join(metadata_path, 'video_10fps_metadata.csv'))
 
 # %%
 # Inspecting the metadata
 
-video_metadata = pd.read_csv(os.path.join(metadata_path, 'video_metadata.csv'), index_col='video')
+video_metadata = pd.read_csv(os.path.join(metadata_path, 'video_10fps_metadata.csv'), index_col='video')
+# Have to drop duplicates using the index, not using .drop_duplicates(), because that ignores the index
+# and only finds duplicates on the row values
+video_metadata = video_metadata[~video_metadata.index.duplicated()]
 
 # Need these because they have the top-1 human class labels attached to the video IDs
 k19_train = pd.read_csv(os.path.join(metadata_path, 'train_video_ids.csv'), index_col='video')
@@ -76,11 +78,11 @@ ck_censored = ck_censored['less.bad'].to_list() + ck_censored['very.bad'].to_lis
 ck_censored = [vid for vid in ck_censored if not pd.isna(vid)]
 
 video_metadata = video_metadata.join(pd.concat([k19_train, k19_test]))
+video_metadata['train'] = video_metadata.index.isin(k19_train.index)
 video_metadata['censored'] = video_metadata.index.isin(ck_censored)
 # %%
 # Copy videos out at 10 fps
 
-fps10_path = '/home/mthieu/Repos/CowenKeltner/videos_10fps'
 for idx, row in tqdm(video_metadata.query('duration <= 11').iterrows()):
     emo_orig_path = os.path.join(stim_path, row['emotion'])
     emo_fps10_path = os.path.join(fps10_path, row['emotion'])
@@ -97,4 +99,25 @@ for idx, row in tqdm(video_metadata.query('duration <= 11').iterrows()):
             .run()
         )
 
+# %%
+# make framewise version of video metadata for framewise torch dataset
+# only using the 10 fps resampled frames from the short ones!
+video_ids = []
+frame_ids = []
+emotions = []
+trains = []
+censoreds = []
+
+for idx, nframes, emotion, train, censored in video_metadata[['nframes', 'emotion', 'train', 'censored']].itertuples():
+    for frame in range(nframes):
+        video_ids.append(idx)
+        frame_ids.append(frame)
+        emotions.append(emotion)
+        trains.append(train)
+        censoreds.append(censored)
+
+frame_metadata = pd.DataFrame({'emotion': emotions, 'train': trains, 'censored': censoreds}, index=[video_ids, frame_ids])
+frame_metadata.index.names = ['video', 'frame']
+
+frame_metadata.to_csv(os.path.join(metadata_path, 'frame_10fps_metadata.csv'))
 # %%
