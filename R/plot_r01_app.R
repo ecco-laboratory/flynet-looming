794b4ae1c510c878d18f8a274957960434d4db3c
monicathieu <mthieu@emory.edu>
2023-02-16 16:02:24 -0500

    add model performance boxplot

--- R/plot_r01_app.R
+++ R/plot_r01_app.R
@@ -8,7 +8,8 @@ require(magrittr)
 # because the stimuli don't hit the face
 video_names <- c(
   "lourenco",
-  "baseball"
+  "baseball",
+  "bower"
 )
 
 hit_probs <- tibble(video = video_names) %>%
@@ -22,10 +23,17 @@ hit_probs <- tibble(video = video_names) %>%
   unnest(data)
 
 hit_probs %>% 
+  mutate(video = fct_recode(video,
+                            "Video of baseball hitting camera" = "baseball",
+                            "Looming shadow modeled on Bower, Broughton, & Moore (1971)" = "bower",
+                            "Looming rabbit from Vagnoni, Lourenco, & Longo (2012)" = "lourenco")) %>% 
   ggplot(aes(x = frame_num, y = phit)) + 
-  geom_line() + 
-  geom_smooth() +
-  facet_wrap(~ video, ncol = 1, scales = "free_x")
+  # geom_line() + # the actual data
+  geom_smooth(color = "black") + # smoothed for public consumption
+  facet_wrap(~ video, ncol = 1, scales = "free_x") +
+  labs(x = "Time elapsed (frames, frame rates differ per video)",
+       y = "Estimated likelihood of eventual collision") +
+  theme_bw()
 
 ## figure 2: FlyNet does ok on retinotopic ring expand in SC ----
 
@@ -57,4 +65,34 @@ metrics_sc %>%
   scale_color_manual(values = c("black", "coral")) +
   guides(x = guide_axis(angle = 30), color = "none") +
   labs(x = "Retinotopic stimulus type", y = "cross-validated R-squared") +
+  theme_bw(base_size = 14)
+
+metrics_sc %>% 
+  select(stim_type, fold_num, pred) %>% 
+  unnest(pred) %>% 
+  filter(split_type == "test") %>% 
+  group_by(stim_type, subj_num) %>% 
+  summarize(cv_r = cor(obs, flynet), .groups = "drop") %>% 
+  # group_by(stim_type) %>% 
+  # summarize(cv_r = mean(correlation), .groups = "drop") %>% 
+  # must separately relevel and recode because recode doesn't change level order
+  mutate(highlight_me = stim_type == "ring_expand",
+         stim_type = fct_relevel(stim_type, 
+                                 "wedge_clock", 
+                                 "wedge_counter", 
+                                 "ring_contract", 
+                                 "ring_expand"),
+         stim_type = fct_recode(stim_type, 
+                                "CW wedge" = "wedge_clock", 
+                                "CCW wedge" = "wedge_counter", 
+                                "Contracting ring" = "ring_contract", 
+                                "Expanding ring" = "ring_expand")) %>% 
+  ggplot(aes(x = stim_type, y = cv_r, color = highlight_me)) + 
+  geom_hline(yintercept = 0, linetype = "dotted") + 
+  geom_hline(yintercept = 0.1, linetype = "dotted", color = "gray60") + 
+  geom_boxplot(alpha = 0.8) + 
+  geom_jitter(alpha = 0.5, width = 0.1) + 
+  scale_color_manual(values = c("black", "coral")) +
+  guides(x = guide_axis(angle = 30), color = "none") +
+  labs(x = "Retinotopic stimulus type", y = "cross-validated r") +
   theme_bw(base_size = 14)
