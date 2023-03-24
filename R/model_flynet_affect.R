83967920d89cf83b311ad968d4e14b40126ec0d5
monicathieu <mthieu@emory.edu>
2023-03-24 14:46:35 -0400

    perm shuffles test, preds outputs probs/classes

--- R/model_flynet_affect.R
+++ R/model_flynet_affect.R
@@ -41,37 +41,56 @@ get_flynet_activation_ck2017 <- function (file, metadata) {
 
 ## fit some fuckin models ----
 
-get_discrim_workflow <- function (in_data, discrim_engine = discrim_linear()) {
+# Separating this from the workflow function
+# Because the workflows pkg does not allow passing the strings_as_factors arg in
+# such that a recipe trained within fit.workflow will coerce the video ID column to factor
+# and then using bake() to drop the predictor columns before binding to new predicted data
+# fails to find the new factor levels of the testing videos bc duh
+get_discrim_recipe <- function (in_data) {
   # remember, data only needs something with the right colnames and types
-  discrim_recipe <- recipe(emotion ~ ., data = head(in_data)) %>% 
+  out <- recipe(emotion ~ ., data = head(in_data)) %>% 
     update_role(video, censored, new_role = "ID") %>% 
     step_rm(split)
   
-  discrim_workflow <- workflow() %>% 
+  return (out)
+}
+
+# UNTRAINED workflow!
+get_discrim_workflow <- function (in_recipe, discrim_engine = discrim_linear()) {
+  
+  out <- workflow() %>% 
     # By default, LDA with engine = "MASS" with discrim_linear()
     # Have to use pls(method = "classification") to get PLS-DA
     add_model(discrim_engine) %>% 
-    add_recipe(discrim_recipe)
+    add_recipe(in_recipe)
   
-  return (discrim_workflow)
+  return (out)
 }
 
 # what do we want? predictions!
 # when do we want them? now!
 # But don't use this for model performance bc we have tidymodels xval for that (below)
-get_discrim_preds_from_trained_model <- function (in_workflow_fit, test_data) {
+get_discrim_preds_from_trained_model <- function (in_workflow_fit, in_recipe, test_data) {
   
-  out <- in_workflow_fit %>% 
+  pred_classes <- in_workflow_fit %>% 
     # "predict" on the HELD OUT data
-    predict(new_data = test_data) %>% 
+    predict(new_data = test_data, type = "class")
+  
+  # Egh we need the class probs to calculate AUC
+  pred_probs <- in_workflow_fit %>% 
+    predict(new_data = test_data, type = "prob")
+  
+  out <- in_recipe %>%
+    # use recipes prep and bake to drop the predictor cols
+    # this should be evergreen to changes in the names of the predictor cols
+    prep(training = NULL, strings_as_factors = FALSE) %>% 
+    bake(test_data, has_role("ID"), all_outcomes()) %>% 
     # bind back onto the original df to recover the observation identifying columns etc
     # apparently this is how they do it in the tidymodels docs too, so there isn't a better way
     # so... dumb
-    bind_cols(test_data, .) %>% 
+    bind_cols(pred_classes, pred_probs) %>% 
     rename(emotion_obs = emotion, emotion_pred = .pred_class) %>% 
-    mutate(emotion_obs = factor(emotion_obs)) %>% 
-    # Fuck the Xs! Just gimme the Ys
-    select(video, censored, starts_with("emotion"))
+    mutate(emotion_obs = factor(emotion_obs))
   
   return (out)
 }
@@ -84,21 +103,21 @@ get_discrim_preds_from_trained_model <- function (in_workflow_fit, test_data) {
 # that the estimate from a single train-test split should be fairly stable/low SE
 # Note that times sets the number of permutation iterations to be run in a single batch
 # and there is no paralleling inside of this function
-perm_beh_metrics <- function (in_split, in_workflow, times) {
+# but since it's not re-fitting any models, it's pretty fast and doesn't NEED to be paralleled
+perm_beh_metrics <- function (in_preds, truth_col, estimate_col, times) {
+  truth_col <- enquo(truth_col)
+  estimate_col <- enquo(estimate_col)
+  # Per Kragel 2019 paper, this takes finished preds,
+  # which effectively keeps the training model the same, without refitting it
   
-  out <- in_split %>% 
-    # Only permute emotion classes for the training data
-    # ne touche pas les testing data. that's for prediction only!
-    training() %>% 
-    permutations(permute = emotion, times = times) %>% 
+  out <- in_preds %>% 
+    # permute the ground truth outcomes before calculating classification "accuracy"
+    permutations(permute = !!truth_col, times = times) %>% 
     # needs to be named using tidymodels tune convention to use tune metrics collecting later
     mutate(.metrics = map(splits, 
                         \(x) x %>% 
                           analysis() %>% 
-                          fit(in_workflow, data = .) %>% 
-                          # testing on the same held-out, NOT PERMUTED, testing data every time
-                          get_discrim_preds_from_trained_model(test_data = testing(in_split)) %>% 
-                          accuracy(truth = emotion_obs, estimate = emotion_pred),
+                          accuracy(truth = !!truth_col, estimate = !!estimate_col),
                           .progress = "permutation! no breathing!"))
   
   return (out)
