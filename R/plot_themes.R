9064f5cc9a8b12d0f1f05477160c9cdc28c08954
monicathieu <mthieu@emory.edu>
2023-04-06 13:12:47 -0400

    init theme helpers

---
+++ R/plot_themes.R
@@ -0,0 +1,16 @@
+# setup, aka helper function definition ----
+
+# this is a targets-compatible function script
+# so no library()-ing here
+
+## theme objects ----
+
+theme_slides <- function (base_size = 12, base_family = "Graphik", ...) {
+  out <- theme_bw() +
+    theme(legend.key = element_blank(),
+          legend.background = element_blank(),
+          axis.line = element_line(color = "grey40"),
+          axis.ticks = element_line(color = "grey40"),
+          plot.background = element_rect(fill = "transparent", color = "transparent")) +
+    theme(...)
+}
