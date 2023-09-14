578e04a6579a33af93937eb18d9db86a42cf60ef
monicathieu <mthieu@emory.edu>
2023-09-14 17:39:47 -0400

    allows approach angle, midpoint start

--- python/myutils/make_looming_video.py
+++ python/myutils/make_looming_video.py
@@ -52,11 +52,25 @@ parser.add_argument(
     help="Distance that object should END AT, in object-widths"
 )
 parser.add_argument(
+    '--posstart',
+    default='end',
+    type=str,
+    choices=['end', 'middle'],
+    help="Start at end or midpoint of trajectory? Defaults to end (far for looming)"
+)
+parser.add_argument(
     '--direction',
     default='looming',
     type=str,
     choices=['looming', 'receding'],
     help="Type of motion? Looming (forwards) or receding (backwards)? Defaults to looming"
+)
+parser.add_argument(
+    '--angle',
+    default='eyelevel',
+    type=str,
+    choices=['eyelevel', 'above', 'below'],
+    help="Starting angle eye level, above, or below? Defaults to eyelevel"
 )
 parser.add_argument(
     '--pausetime',
@@ -93,18 +107,80 @@ args = vars(parser.parse_args())
 
 # %%
 # Helper functions for the loom resizer
-def scale_image_by_viewing_distance(image, diameter_initial, distance_initial, distance_current):
+def scale_image_by_viewing_distance(image, diameter_initial, distance_screen, distance_current):
     # per Sun and Frost 1998 appendix (THANK GOD),
     # theta_t = 2 * arctan(diameter_initial / (2 * distance_t))
 
     # first calculate theta using the fake distance of the looming object
     theta = 2 * np.arctan2(diameter_initial / 2, distance_current)
     # then calculate the drawn diameter reapplying theta back to the screen distance
-    diameter = 2 * np.tan(theta / 2) * distance_initial
-    scale = diameter / img.width
+    diameter = 2 * np.tan(theta / 2) * distance_screen
+    scale = diameter / image.width
     # use ImageOps.scale instead of Image.resize bc scale always retains aspect ratio
     image = ImageOps.scale(image, scale)
     return image
+
+def scale_and_fit_oblique(image, angle, frame_size, diameter_initial, distance_far, distance_current):
+    # the distance values come in as normal distance
+    # so get them in x and y given that the item is approaching at an angle
+    # assume approaching from 30 degrees above or below the horizon
+    approach_angle = np.pi/15
+    distance_x_far = distance_far * np.cos(approach_angle)
+    distance_x_current = distance_current * np.cos(approach_angle)
+    distance_y_current = distance_current * np.sin(approach_angle)
+    
+    # get the visual angle of the upper and lower edges of the image
+    theta_upper = np.arctan((distance_y_current + diameter_initial/2) / distance_x_current)
+    theta_lower = np.arctan((distance_y_current - diameter_initial/2) / distance_x_current)
+    
+    # project those back out to get screen heights for the upper and lower edges
+    coord_upper = np.tan(theta_upper) * distance_x_far
+    coord_lower = np.tan(theta_lower) * distance_x_far
+    height = coord_upper - coord_lower
+    
+    # expand the whole thing based on the height projection
+    scale = height / image.height
+    image_scaled = ImageOps.scale(image, scale)
+    
+    # prepare to position the object at the top or bottom of the hotdog-padded image
+    if angle == 'above':
+        long_centering = 0
+    else:
+        long_centering = 1
+
+    # pad the height to get an image that is symmetrical about eye level
+    image_long = ImageOps.pad(
+        image_scaled,
+        size=(image_scaled.width, int(coord_upper*2)),
+        centering=(0.5, long_centering)
+    )
+    # now pad/trim the height to get it to screen height
+    # this has to be done in an annoying way bc the image is not centered in the screen frame
+    if image_long.height > frame_size[1]:
+        image_long = ImageOps.fit(
+            image_long,
+            size=(image_long.width, frame_size[1])
+        )
+    elif image_long.height < frame_size[1]:
+        image_long = ImageOps.pad(
+            image_long,
+            size=(image_long.width, frame_size[1])
+        )
+    
+    if image_long.width > frame_size[0]:
+        image_wide = ImageOps.fit(
+            image_long,
+            size=frame_size
+        )
+    elif image_long.width < frame_size[0]:
+        image_wide = ImageOps.pad(
+            image_long,
+            size=frame_size
+        )
+    else:
+        image_wide = image_long
+    
+    return image_wide
 
 def fit_image_to_frame(image, frame_size):
     # first calculate aspect ratio of the input image before it gets resized
@@ -146,10 +222,29 @@ img = img.convert('RGB')
 # Assume constant velocity approaching object
 
 # if 25 fps, each frame is 40 ms
-diameter_initial = args['objwidth'] # in px, basically arbitrary bc this would be "real world" obj size
-distance_initial = diameter_initial * args['diststart'] # Default 18 obj-widths away from screen? sure
-distance_final = diameter_initial * args['distend'] # default 0 obj-widths, or "full collision"
 direction = args['direction']
+angle = args['angle']
+start_location = args['posstart']
+
+# setting distances based on start location and movement direction
+diameter_far = args['objwidth'] # in px, basically arbitrary bc this would be "real world" obj size
+distance_far = diameter_far * args['diststart'] # Default 18 obj-widths away from screen? sure
+distance_near = diameter_far * args['distend'] # default 0 obj-widths, or "full collision"
+distance_middle = (distance_far + distance_near) / 2
+if start_location == 'middle':
+    distance_initial = distance_middle
+    if direction == 'looming':
+        distance_final = distance_near
+    elif direction == 'receding':
+        distance_final = distance_far
+elif start_location == 'end':
+    if direction == 'looming':
+        distance_initial = distance_far
+        distance_final = distance_near
+    elif direction == 'receding':
+        distance_initial = distance_near
+        distance_final = distance_far
+
 duration_wait_sec = args['pausetime'] # in seconds
 duration_loom_sec = args['loomtime'] # in seconds
 fps = args['fps']
@@ -158,9 +253,10 @@ duration_wait_frames = np.floor(duration_wait_sec * fps).astype(int)
 duration_loom_frames = np.ceil(duration_loom_sec * fps).astype(int)
 # velocity in px/frame, weird units but I think it will work for now
 # assuming very-near collision occurs at the end of the video duration
+
 # the arbitrary looking adjustment is to get it so distance is never 0
 # because that causes PIL to overflow when attempting to resize the image to some HUGE size
-velocity = ((distance_initial-distance_final)-10) / (duration_loom_frames-1)
+speed = (np.abs(distance_initial-distance_final)-10) / (duration_loom_frames-1)
 # default is literally the monitor size in my office, just for testing
 screen_size = tuple(args['framesize'])
 
@@ -170,14 +266,20 @@ frames = []
 # Generate the actual motion sequence
 for frame in trange(duration_loom_frames, desc='Expanding images to loom'):
     # current distance = initial distance - distance traveled since then
-    distance = distance_initial - velocity*frame
-    this_img = scale_image_by_viewing_distance(img, diameter_initial, distance_initial, distance)
-    this_img = fit_image_to_frame(this_img, screen_size)
-    frames.append(this_img)
+    # + for receding
+    # now that this generates for receding too
+    # no longer need to flip the frame sequence for receding
+    if direction == 'looming':
+        distance = distance_initial - speed*frame
+    elif direction == 'receding':
+        distance = distance_initial + speed*frame
 
-# Put my thang down flip it and reverse it
-if direction == 'receding':
-    frames.reverse()
+    if angle == 'eyelevel':
+        this_img = scale_image_by_viewing_distance(img, diameter_far, distance_far, distance)
+        this_img = fit_image_to_frame(this_img, screen_size)
+    else:       
+        this_img = scale_and_fit_oblique(img, angle, screen_size, diameter_far, distance_far, distance)
+    frames.append(this_img)
 
 # Put the waiting frames on
 # This way it always waits at the beginning of the video itself
@@ -191,6 +293,8 @@ if duration_wait_frames > 0:
 video_name = [
     os.path.splitext(os.path.split(args['infile'])[1])[0], 
     direction, 
+    start_location,
+    angle,
     'pause' + '{:02.1f}'.format(args['pausetime']), 
     'loom' + '{:02.1f}'.format(args['loomtime'])
 ]
