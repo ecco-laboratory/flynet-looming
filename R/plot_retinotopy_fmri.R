802038b30d52e61b52ff79b87a597672576339a4
monicathieu <mthieu@emory.edu>
2024-03-04 20:50:20 -0500

    plot rev1 boxplot mini instead

--- R/plot_retinotopy_fmri.R
+++ R/plot_retinotopy_fmri.R
@@ -30,42 +30,59 @@ plot_flynet_activations_convolved <- function (activations, run_types = NULL) {
   return (out)
 }
 
-plot_boxplot_cv_r_studyforrest <- function (metrics_sc,
-                                            metrics_v1) {
-  out <- bind_rows(SC = metrics_sc,
-                   V1 = metrics_v1,
-              .id = "roi") %>% 
-    select(roi, fit_type, model_type, perf) %>% 
-    unnest(perf) %>% 
-    group_by(roi, fit_type, model_type, stim_type, subj_num) %>% 
-    summarize(cv_r = mean(r_model), .groups = "drop") %>% 
-    # must separately relevel and recode because recode doesn't change level order
-    mutate(stim_type = fct_relevel(stim_type, 
-                                   "wedge_clock", 
-                                   "wedge_counter", 
-                                   "ring_contract", 
-                                   "ring_expand"),
-           stim_type = fct_recode(stim_type, 
-                                  "CW wedge" = "wedge_clock", 
-                                  "CCW wedge" = "wedge_counter", 
-                                  "Contracting ring" = "ring_contract", 
-                                  "Expanding ring" = "ring_expand"),
-           model_type = fct_relevel(model_type,
-                                    "Inverse tau",
-                                    "Eta",
-                                    "Inverse tau and eta",
-                                    "Collision detection model",
-                                    "Collision detection + inverse tau",
-                                    "Collision detection + eta",
-                                    "Collision detection + inverse tau and eta")) %>% 
-    ggplot(aes(x = model_type, y = cv_r, fill = fit_type, color = fit_type)) + 
+plot_boxplot_mini_cv_r_studyforrest <- function (metrics_all, this_roi = "SC") {
+  
+  plot_data <- metrics_all %>% 
+    filter(roi == this_roi, parameter == "flynet") %>% 
+    mutate(is_expand = if_else(stim_type == "ring_expand", "Expanding rings", "Other stimuli")) %>% 
+    pivot_longer(cols = starts_with("r_model"), 
+                 names_to = "fit_type", values_to = "r_model", names_prefix = "r_model_") %>% 
+    group_by(is_expand, fit_type, subj_num) %>% 
+    summarize(r_model = mean(r_model), .groups = "drop") %>% 
+    mutate(fit_type = fct_recode(fit_type, "Stimulus-general" = "overall", "Stimulus-specific" = "by.run.type"))
+  
+  plot_summaries <- plot_data %>%
+    group_by(is_expand, fit_type) %>% 
+    summarize(across(r_model, list(mean = mean, sd = sd, se = \(x) sd(x)/sqrt(length(x)))))
+  
+  out <- plot_data %>% 
+    ggplot(aes(x = fit_type, y = r_model)) + 
+    geom_line(aes(group = subj_num), color = "grey60") + 
+    geom_jitter(width = 0.03, color = "grey60", size = 2, alpha = 0.7) +
+    geom_errorbar(aes(y = NULL,
+                      ymin = r_model_mean - 2*r_model_se,
+                      ymax = r_model_mean + 2*r_model_se),
+                  data = plot_summaries, width = 0) +
+    geom_line(aes(group = 1, y = r_model_mean), 
+              data = plot_summaries) +
+    geom_point(aes(y = r_model_mean), 
+               data = plot_summaries, size = 3) +
     geom_hline(yintercept = 0, linetype = "dotted") + 
-    geom_boxplot(alpha = 0.8) + 
-    # geom_line(aes(group = subj_num), alpha = 0.5) +
-    geom_point(alpha = 0.5) +
-    facet_grid(roi ~ stim_type) +
-    guides(x = guide_axis(angle = 30), color = "none") +
-    labs(x = "Retinotopic stimulus type", y = "Cross-validated r")
+    scale_color_viridis_c(direction = -1) +
+    facet_grid(~ is_expand) +
+    labs(x = "Model type", y = "Cross-validated r")
+
+  return (out)
+}
+
+plot_intxn_cv_r_studyforrest <- function (metrics_all, this_roi = "SC") {
+  preplot <- metrics_all %>% 
+    filter(roi == this_roi, parameter == "flynet") %>% 
+    mutate(r_diff = r_model_by.run.type - r_model_overall, 
+           is_expand = if_else(stim_type == "ring_expand", "Expanding rings", "Other stimuli")) %>% 
+    group_by(is_expand, subj_num) %>% 
+    summarize(r_diff = mean(r_diff))
+  
+  out <- preplot %>% 
+    summarize(mean_r_diff = mean(r_diff), 
+              se_diff = sd(r_diff)/sqrt(n())) %>% 
+    ggplot(aes(x = is_expand, y = mean_r_diff)) + 
+    geom_line(aes(y = r_diff, group = subj_num), data = preplot, color = "grey60") + 
+    geom_jitter(aes(y = r_diff), data = preplot, width = 0.02, color = "grey60", size = 2, alpha = 0.7) +
+    geom_hline(yintercept = 0, linetype = "dotted") +
+    geom_errorbar(aes(ymin = mean_r_diff - 2*se_diff, ymax = mean_r_diff + 2*se_diff), width = 0) +
+    geom_point(size = 3) +
+    labs(x = NULL, y = expression(r[specific] - r[general]))
   
   return (out)
 }
